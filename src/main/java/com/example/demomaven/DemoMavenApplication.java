98143377eb9a5d9c42a7b24d090461273ba0b6bb
manuel <lenguaje100natural@gmail.com>
2023-03-26 15:27:36 -0300

    - Se crea clase Carro - Se agrega setId y getId - Se instancia objetos de clase Carro - Se invoca la funcion acelerar desde objetos - Se imprime objetos

--- src/main/java/com/example/demomaven/DemoMavenApplication.java
+++ src/main/java/com/example/demomaven/DemoMavenApplication.java
@@ -1,5 +1,6 @@
 package com.example.demomaven;
 
+import com.example.demomaven.domain.Carro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,18 @@ public class DemoMavenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMavenApplication.class, args);
 		System.out.println("Hola Mundo!!, estoy aprendiendo Java!");
+
+		Carro objetoCarro = new Carro();
+		Carro objetoCarroConId = new Carro("Carro con Id");
+
+		objetoCarro.setId("Carro sin Id, se cargo desde SetId");
+
+		objetoCarro.acelerar(1);
+		objetoCarroConId.acelerar(2);
+
+		System.out.println(objetoCarroConId);
+		System.out.println(objetoCarro);
+
 	}
 
 }
